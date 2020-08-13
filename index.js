b901fe82427702fcecf31dc6430894ccd22b8b8b
Jerry Sewell <jerry.sewell@gmail.com>
2020-08-13 11:17:34 -0700

    changed greeting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-})
+app.get("/", (req, res) => {
+  res.send({ bye: "buddy" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
