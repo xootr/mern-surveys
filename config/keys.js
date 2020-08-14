6eed882e8b1e04828c1d87023082f20c7f328693
Jerry Sewell <jerry.sewell@gmail.com>
2020-08-14 16:13:31 -0700

    fixed bug on NODE_ENV

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //keys.js -- figure out what env we are running in
-if (process.send.NODE_EV == 'production') {
+if (process.send.NODE_ENV == 'production') {
     // we are in prod
     module.exports = require('./prod');
 } else {
