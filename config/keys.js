8950ad95df86ff1b62e450e2b227bc0574240ba4
Jerry Sewell <jerry.sewell@gmail.com>
2020-08-14 16:08:37 -0700

    finished auth

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//keys.js -- figure out what env we are running in
+if (process.send.NODE_EV == 'production') {
+    // we are in prod
+    module.exports = require('./prod');
+} else {
+    // we are in dev
+    module.exports = require('./dev');
+}
